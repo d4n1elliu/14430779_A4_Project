f81af0d9e0e794b4f49806fd9f43a00c60db6dd3
Daniel Liu <daniel.liu8750@gmail.com>
2023-11-01 02:05:10 +1100

    Commit

---
+++ Assets/Scripts/PacStudentController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed at which PacStudent moves
+    private Vector3 targetPos;
+    private Vector3 nextGridPos;
+    private Vector3 lastInputDir = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
